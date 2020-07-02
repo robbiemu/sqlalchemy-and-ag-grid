b4394f519e83a06e8244ce94ba437df946a2390d
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2020-07-02 18:14:53 -0400

    backtracking in the creation of the solved puzzle

--- sudoku/generator.py
+++ sudoku/generator.py
@@ -22,13 +22,18 @@ class SudokuCel:
             return
         choices = list(self.seed_numbers)
         random.shuffle(choices)
-        self.setN(choices.pop())
+        for choice in choices:
+            if not self.delegate.check_backtrack(choice, self.x, self.y):
+                self.setN(choice)
+                break
 
     def eliminate(self, m):
         '''remove a number from seed_numbers'''
         self.seed_numbers.discard(m)
         if len(self.seed_numbers) == 1:
-            self.setN(self.seed_numbers.pop())
+            choice = self.seed_numbers.pop()
+            if not self.delegate.check_backtrack(choice, self.x, self.y):
+                self.setN(choice)
 
     def setN(self, n):
         '''helper method to set n and broadcast in correct order'''
@@ -46,23 +51,76 @@ class SudokuGenerator:
     '''generate a (completed) puzzle with this class'''
 
     cels = np.empty(shape=(9, 9), dtype=object)
+    track = set()
+    resolve = False
+    retrace = 1
 
     def generate(self):
         '''a roughly hewn method for generating sudoku by:
         1. initializing an array of cels
         2. for each cel in the array, resolve it
-        3. route broadcasts to appropriate cels so they can self-trim and resolve
+        3. route broadcasts to appropriate cels so they can self-trim and resolve, backtracking as needed
         '''
         for row in range(9):
             for col in range(9):
                 self.cels[row, col] = SudokuCel(row, col, self)
+
+        while True:
+            self.retrace = 1
+            self.solve()
+            if not self.resolve:
+                break
+
+    def solve(self):
+        self.resolve = False
+        self.retrace = self.retrace + 1
         for row in range(9):
             for col in range(9):
                 if self.cels[row, col].n is None:
-                    self.cels[row, col].resolve()
+                    if len(self.cels[row, col].seed_numbers) > 0:
+                        self.cels[row, col].resolve()
+                        if self.cels[row, col].n is None:
+                            self.backtrack()
+                    else:
+                        print('illegal state', self.cels, len(self.track))
+                        if len(self.track) > 0:
+                            self.backtrack()
+
+    def backtrack(self):
+        for step in range(self.retrace):
+            if len(self.track) == 0:
+                return
+            self.resolve = True
+
+            row, col = self.track.pop()
+
+            self.cels[row, col].n = None
+            for r in range(9):
+                for i in range(9):
+                    for c in range(9):
+                        self.cels[r, c].seed_numbers = {
+                            1, 2, 3, 4, 5, 6, 7, 8, 9}
+                        for j in range(9):
+                            if i != r and self.cels[i, c].n != None:
+                                self.cels[r, c].seed_numbers.discard(
+                                    self.cels[i, c].n)
+                            if i != c and self.cels[r, i].n != None:
+                                self.cels[r, c].seed_numbers.discard(
+                                    self.cels[r, i].n)
+
+            for r in range(9):
+                for c in range(9):
+                    x = r - r % 3
+                    y = c - c % 3
+                    for i in range(x, x + 3):
+                        for j in range(y, y + 3):
+                            if not (i == r and j == c) and self.cels[i, j].n != None:
+                                self.cels[r, c].seed_numbers.discard(
+                                    self.cels[i, j].n)
 
     def broadcast(self, n, row, col):
         '''tell each row column and grid member that is not this member that a member has resolved this number, so they can eliminate that number'''
+        self.track.add((row, col))
         for i in range(9):
             if i != row:
                 self.cels[i, col].eliminate(n)
@@ -72,8 +130,23 @@ class SudokuGenerator:
         y = col - col % 3
         for i in range(x, x + 3):
             for j in range(y, y + 3):
-                if i != row and j != col:
+                if not (i == row and j == col):
                     self.cels[i, j].eliminate(n)
+
+    def check_backtrack(self, n, row, col):
+        '''tell this member if any row column or grid member that is not this member has resolved this number, so they can eliminate that number'''
+        for i in range(9):
+            if i != row and self.cels[i, col].n == n:
+                return True
+            if i != col and self.cels[row, i].n == n:
+                return True
+        x = row - row % 3
+        y = col - col % 3
+        for i in range(x, x + 3):
+            for j in range(y, y + 3):
+                if i != row and j != col and self.cels[i, j].n == n:
+                    return True
+        return False
 
     def get_grid(self):
         '''return the numpy int grid of our cels'''
