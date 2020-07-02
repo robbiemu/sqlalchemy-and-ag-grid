0b41cb6e40d6e267e1ba634300e108b34cd01854
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2020-07-02 15:04:09 -0400

    debug ui

--- client/src/util/sudoku-parser.ts
+++ client/src/util/sudoku-parser.ts
@@ -25,10 +25,15 @@ export const sudokuParser = {
     indices.forEach(x => game.push([]))
 
     indices.forEach((x, i) =>
-      indices.forEach((y, j) => (game[i][j] = puzzle[x + y]))
+      indices.forEach((y, j) => {
+        const a = Math.floor(i / 3) * 3 + Math.floor(j / 3)
+        const b = (i % 3) * 3 + (j % 3)
+        game[a][b] = puzzle[x + y]
+      })
     )
+    console.log('game', JSON.parse(JSON.stringify(game)))
 
-    masks.forEach((mask: Mask) => delete game[mask.x][mask.y])
+    masks.forEach((mask: Mask) => delete game[mask.y][mask.x])
 
     return game
   }
